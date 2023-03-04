dbebb1fdd914cb6aa137928dc9f1285aaed02c45
miltiades-the-general <miltiadesgeneral@gmail.com>
2023-03-04 12:43:04 -0800

    Implemented random choice and reconfigured randomword

--- randleWord.py
+++ randleWord.py
@@ -64,118 +64,127 @@ class randleWord():
         Returns a single word from the word_dict given a query of wordLength, or from minWordLength - maxWordLength.
         If wordLength is specified then the range from minWordLength, maxWordLength is ignored
         """
-        if not isinstance(wordLength, int):
-            raise TypeError(f"wordLength must be of type int not {type(wordLength)}")
-        
-        if not isinstance(minWordLength, int):
-            raise TypeError(f"minWordLength must be of type int not {type(wordLength)}")
-    
-        if not isinstance(maxWordLength, int):
-            raise TypeError(f"maxWordLength must be of type int not {type(wordLength)}")
-        
-        if not isinstance(startsWith, str):
-            raise TypeError(f"startsWith must be of type str not {type(startsWith)}")
-        
-        if wordLength < 0:
-            raise ValueError(f"wordLength must be a positive value or 0")
-        
-        if wordLength > 31:
-            raise ValueError(f"Length {wordLength} exceeds the max length. The longest word in the dictionary is of length 31")
+        self.__catch_type_and_value_errors(wordLength, minWordLength, maxWordLength, startsWith)
 
         # Simple case if no wordLength is specified
-        if wordLength == 0:
+        if not wordLength:
             # Everything is default
             if minWordLength == 0 and maxWordLength == 32 and startsWith == '': 
-                _rand_index = random.randrange(0, self.dict_length)
-                return self.word_dict[str(_rand_index)]
+                rand_index = random.randrange(0, self.dict_length)
+                return self.word_dict[str(rand_index)]
             
             # only minWordLength is specified
             elif minWordLength != 0 and maxWordLength == 32 and startsWith == '':
-                _sorted_word_array = self.__dct_sorted_array_by_len()
-                _sorted_len_array = self.__len_sorted_array_by_len(_sorted_word_array)
-                _min_range = self.__find_left_bound(_sorted_len_array, minWordLength, None)
-                _rand_index = random.randrange(_min_range, len(_sorted_word_array))
-                return _sorted_word_array[_rand_index]
+                sorted_word_array, min_range = self.__sorted_array_and_find_length_range(_minWordLength=minWordLength)
+                rand_index = random.randrange(min_range, len(sorted_word_array))
+                return sorted_word_array[rand_index]
 
             # only maxWordLength is specified
             elif maxWordLength != 32 and minWordLength == 0 and startsWith == '':
-                _sorted_word_array = self.__dct_sorted_array_by_len()
-                _sorted_len_array = self.__len_sorted_array_by_len(_sorted_word_array)
-                _max_range = self.__find_right_bound(_sorted_len_array, maxWordLength, None)
-                _rand = random.seed(self.seed)
-                _rand_index = _rand.randrange(0, _max_range)
-                return _sorted_word_array[_rand_index]
+                sorted_word_array, max_range = self.__sorted_array_and_find_length_range(_maxWordLength=maxWordLength)
+                rand_index = random.randrange(0, max_range)
+                return sorted_word_array[rand_index]
             
-            # minWordLength is specified and startsWith is specified
-            elif minWordLength != 0 and startsWith != '' and maxWordLength == 0:
+            # minWordLength and or maxWordLength is specified and startsWith is specified
+            elif (minWordLength != 0 or maxWordLength != 32) and startsWith != '':
                 # find length range
-                _sorted_word_array = self.__dct_sorted_array_by_len()
-                _sorted_len_array = self.__len_sorted_array_by_len(_sorted_word_array)
-                _min_range = self.__find_left_bound(_sorted_len_array, minWordLength, None)
-                
-                # find alphabetic range
-                _sorted_word_array = self.__dct_sorted_array_by_alph(_sorted_word_array[_min_range:])
-                _sorted_len_array = self.__alph_sorted_array_by_alph(_sorted_word_array)
-                _min_range = self.__find_left_bound(_sorted_len_array, None, startsWith)
+                sorted_word_array, min_range = self.__sorted_array_and_find_length_range(_minWordLength=minWordLength)
+                _, max_range = self.__sorted_array_and_find_length_range(_maxWordLength=maxWordLength)
 
-                _rand_index = random.randrange(_min_range, self.dict_length)
-                return _sorted_word_array[_rand_index]
+                # find alphabetic range
+                sorted_word_array, min_range, max_range = self.__sorted_array_and_find_alph_range(sorted_word_array, _startsWith=startsWith, _minRange=min_range, _maxRange=max_range)
+
+                rand_index = random.randrange(min_range, max_range)
+                return sorted_word_array[rand_index]   
+
+        # If wordLength is explicity specified, ignore the ranges
+        else:
+            if startsWith == '':
+                sorted_word_array, min_range = self.__sorted_array_and_find_length_range(_minWordLength=wordLength)
+                _, max_range = self.__sorted_array_and_find_length_range(_maxWordLength=wordLength)
+                rand_index = random.randrange(min_range, max_range)
+                return sorted_word_array[rand_index]
+            
+            else:
+                sorted_word_array, min_range = self.__sorted_array_and_find_length_range(_minWordLength=wordLength)
+                _, max_range = self.__sorted_array_and_find_length_range(_maxWordLength=wordLength)
+
+                # return the full range
+                sorted_word_array, min_range, max_range = self.__sorted_array_and_find_alph_range(sorted_word_array, startsWith, _minRange=min_range, _maxRange=max_range)
+
+                rand_index = random.randrange(min_range, max_range)
+                return sorted_word_array[rand_index]
+
+    def randWordChoices(self, choiceLength, wordLength=None, minWordLength=0, maxWordLength=32, startsWith=''):
+        """
+        Returns a choice of random words given a sampleLength, or size of array, wordLength, or minWordLength, maxWordLength
+        """
+        if not isinstance(choiceLength, int):
+            raise TypeError(f"sampleLength must be of type int. You supplied {type(choiceLength)}")
+        
+        self.__catch_type_and_value_errors(wordLength, minWordLength, maxWordLength, startsWith)
+
+        word_choices = set()
+        # Simple case if no wordLength is specified
+        if not wordLength:
+            # Everything is default
+            if minWordLength == 0 and maxWordLength == 32 and startsWith == '': 
+                while len(word_choices) < choiceLength:
+                    rand_index = random.randrange(0, self.dict_length)
+                    word_choices.add(self.word_dict[str(rand_index)])
+                return word_choices
+            
+            # only minWordLength is specified
+            elif minWordLength != 0 and maxWordLength == 32 and startsWith == '':
+                sorted_word_array, min_range = self.__sorted_array_and_find_length_range(_minWordLength=minWordLength)
+                while len(word_choices) < choiceLength:
+                    rand_index = random.randrange(min_range, len(sorted_word_array))
+                    word_choices.add(sorted_word_array[rand_index])
+                return word_choices
+
+            # only maxWordLength is specified
+            elif maxWordLength != 32 and minWordLength == 0 and startsWith == '':
+                sorted_word_array, max_range = self.__sorted_array_and_find_length_range(_maxWordLength=maxWordLength)
+                while len(word_choices) < choiceLength:
+                    rand_index = random.randrange(0, max_range)
+                    word_choices.add(sorted_word_array[rand_index])
+                return word_choices
             
             # minWordLength is specified and startsWith is specified
-            elif maxWordLength != 32 and startsWith != '' and minWordLength == 0:
-                _sorted_word_array = self.__dct_sorted_array_by_len()
-                _sorted_len_array = self.__len_sorted_array_by_len(_sorted_word_array)
-                _max_range = self.__find_right_bound(_sorted_len_array, maxWordLength, None)
-                
-                _sorted_word_array = self.__dct_sorted_array_by_alph(_sorted_word_array[:_max_range])
-                _sorted_len_array = self.__alph_sorted_array_by_alph(_sorted_word_array)
-                _max_range = self.__find_right_bound(_sorted_len_array, None, startsWith)
-
-                _rand_index = random.randrange(0, _max_range)
-                return _sorted_word_array[_rand_index]
+            elif (minWordLength != 0 or maxWordLength != 32) and startsWith != '':
+                # find length range
+                sorted_word_array, min_range = self.__sorted_array_and_find_length_range(_minWordLength=minWordLength)
+                _, max_range = self.__sorted_array_and_find_length_range(_maxWordLength=maxWordLength)
+                # find alphabetic range
+                sorted_word_array, min_range, max_range = self.__sorted_array_and_find_alph_range(sorted_word_array, _startsWith=startsWith, _minRange=min_range, _maxRange=max_range)
+                while len(word_choices) < choiceLength:
+                    rand_index = random.randrange(min_range, max_range)
+                    word_choices.add(sorted_word_array[rand_index])
+                return word_choices
         
         # If wordLength is explicity specified, ignore the ranges
         else:
             if startsWith == '':
-                _sorted_word_array = self.__dct_sorted_array_by_len()
-                _sorted_len_array = self.__len_sorted_array_by_len(_sorted_word_array)
-                _min_range = self.__find_left_bound(_sorted_len_array, wordLength, None)
-                _max_range = self.__find_right_bound(_sorted_len_array, wordLength, None)
-                _rand_index = random.randrange(_min_range, _max_range)
-                return _sorted_word_array[_rand_index]
+                sorted_word_array, min_range = self.__sorted_array_and_find_length_range(_minWordLength=wordLength)
+                _, max_range = self.__sorted_array_and_find_length_range(_maxWordLength=wordLength)
+                while len(word_choices) < choiceLength:
+                    rand_index = random.randrange(min_range, max_range)
+                    word_choices.add(sorted_word_array[rand_index])
+                return word_choices
             
             else:
-                _sorted_word_array = self.__dct_sorted_array_by_len()
-                _sorted_len_array = self.__len_sorted_array_by_len(_sorted_word_array)
-                _min_range = self.__find_left_bound(_sorted_len_array, wordLength, None)
-                _max_range = self.__find_right_bound(_sorted_len_array, wordLength, None)
+                sorted_word_array, min_range = self.__sorted_array_and_find_length_range(_min_wordLength=wordLength)
+                _, max_range = self.__sorted_array_and_find_length_range(_maxWordLength=wordLength)
                 
-                _sorted_word_array = self.__dct_sorted_array_by_alph(_sorted_word_array[_min_range:_max_range])
-                _sorted_len_array = self.__alph_sorted_array_by_alph(_sorted_word_array)
-                _min_range = self.__find_left_bound(_sorted_len_array, None, startsWith)
-                _max_range = self.__find_right_bound(_sorted_len_array, None, startsWith)
-
-                _rand_index = random.randrange(_min_range, _max_range)
-                return _sorted_word_array[_rand_index]
-
-    def randWordSample(self, sampleLength, wordLength, minWordLength, maxWordLength, startsWith):
-        """
-        Returns a sample of random words given a sampleLength, or size of array, wordLength, or minWordLength, maxWordLength
-        """
-        if not isinstance(sampleLength, int):
-            raise TypeError(f"sampleLength must be of type int. You supplied {type(sampleLength)}")
-        
-        if not isinstance(wordLength, int):
-            raise TypeError(f"wordLength must be of type int not {type(wordLength)}")
-        
-        if not isinstance(minWordLength, int):
-            raise TypeError(f"minWordLength must be of type int not {type(wordLength)}")
+                # return the full range
+                sorted_word_array, min_range, max_range = self.__sorted_array_and_find_alph_range(sorted_word_array, startsWith, _minRange=min_range, _maxRange=max_range)
+                while len(word_choices) < choiceLength:
+                    rand_index = random.randrange(min_range, max_range)
+                    word_choices.add(sorted_word_array[rand_index])
+                return word_choices
     
-        if not isinstance(maxWordLength, int):
-            raise TypeError(f"maxWordLength must be of type int not {type(wordLength)}")
-        
-        if not isinstance(startsWith, str):
-            raise TypeError(f"startsWith must be of type str not {type(startsWith)}")
+    def randWordSample(self):
+        return 
     
     def __find_left_bound(self, _dct_array, _minWordLength, _startsWith):
         """
@@ -187,7 +196,7 @@ class randleWord():
         if _minWordLength:
             _left = bisect.bisect_left(_dct_array, _minWordLength)
 
-        # If the array is being queried by the starting letter
+        # If the array is being queried by the starting letter, find the alphabetic insertion index
         else: 
             _startsWith = ord(_startsWith) - ord("a")
             _left = bisect.bisect_left(_dct_array, _startsWith)
@@ -246,6 +255,47 @@ class randleWord():
             _alph_array.append(_ord)
         
         return _alph_array
+    
+    # abstract the randword function
+    def __sorted_array_and_find_length_range(self, _minWordLength=None, _maxWordLength=None):
+        if _minWordLength:
+            _sorted_word_array = self.__dct_sorted_array_by_len()
+            _sorted_len_array = self.__len_sorted_array_by_len(_sorted_word_array)
+            _min_range = self.__find_left_bound(_sorted_len_array, _minWordLength, None)
+            return _sorted_word_array, _min_range 
+        
+        if _maxWordLength:
+            _sorted_word_array = self.__dct_sorted_array_by_len()
+            _sorted_len_array = self.__len_sorted_array_by_len(_sorted_word_array)
+            _max_range = self.__find_right_bound(_sorted_len_array, _maxWordLength, None)
+            return _sorted_word_array, _max_range
+    
+    def __sorted_array_and_find_alph_range(self, _sorted_word_array, _startsWith, _minRange=0, _maxRange=int(500000)):
+        _sorted_word_array = self.__dct_sorted_array_by_alph(_sorted_word_array[_minRange:_maxRange])
+        _sorted_len_array = self.__alph_sorted_array_by_alph(_sorted_word_array)
+        _min_range = self.__find_left_bound(_sorted_len_array, None, _startsWith)
+        _max_range = self.__find_right_bound(_sorted_len_array, None, _startsWith)
+        return _sorted_word_array, _min_range, _max_range
+
+    def __catch_type_and_value_errors(self, wordLength, minWordLength, maxWordLength, startsWith):
+        if wordLength and not isinstance(wordLength, int):
+            raise TypeError(f"wordLength must be of type int not {type(wordLength)}")
+        
+        if not isinstance(minWordLength, int):
+            raise TypeError(f"minWordLength must be of type int not {type(wordLength)}")
+    
+        if not isinstance(maxWordLength, int):
+            raise TypeError(f"maxWordLength must be of type int not {type(wordLength)}")
+        
+        if not isinstance(startsWith, str):
+            raise TypeError(f"startsWith must be of type str not {type(startsWith)}")
+        
+        if (wordLength and wordLength < 0) or maxWordLength < 0 or minWordLength < 0:
+            raise ValueError(f"wordLength must be a positive value or 0")
+        
+        if (wordLength and wordLength > 31) or minWordLength > 31:
+            raise ValueError(f"Length {wordLength} exceeds the max length. The longest word in the dictionary is of length 31")
+        
 
     def seed(self, *args, **kwds):
         "Stub method.  Not used for a system random number generator."
